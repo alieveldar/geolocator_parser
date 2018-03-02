276c9235d85402a353be7b4275a06bb5dd37a725
alieveldar <illuminatiday@gmail.com>
2018-03-02 18:52:01 +0300

    Points in Kazahstan????

--- main.rb
+++ main.rb
@@ -2,35 +2,44 @@ require 'net/http'
 require 'json'
 require 'pp'
 
-uri = URI('https://geocode-maps.yandex.ru/1.x/?')
-params = { :format => "json", :geocode => "Москва,+улица+Новый+Арбат,+дом 24"}
+def getLocation(address)
+  points_position = []
+  geo_coder_csv_row = []
+  points_address = address
+  uri = URI('https://geocode-maps.yandex.ru/1.x/?')
+  params = {:format => "json", :geocode => points_address[0][0]}
+  uri.query = URI.encode_www_form(params)
+  res = Net::HTTP.get_response(uri)
+  puts res
+
+  if res.is_a?(Net::HTTPSuccess)
+    point_json = res.body
+    point_json = JSON.parse(point_json)
+    points_position << point_json["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"]["pos"].split(" ")
+    geo_coder_csv_row << (points_position[0][0].to_s + ";" + points_position[0][1].to_s + ";" + points_address[0][1].to_s.chomp! + ";" + points_address[0][0].to_s)
+    print geo_coder_csv_row
+  else
+    puts "SOME NO GOOD"
+  end
+  return geo_coder_csv_row
+end
+geo_code_csv = []
 points_address = []
-points_position = []
-zip = []
-count_lines = 0
 filein = File.new("./kzn.csv")
 fileout = File.new("./togeo.csv", 'w')
-  filein.each do |line|
+filein.each do |line|
     points_address << line.split(";")
-    params = {:format => "json", :geocode => points_address[0][0].to_s}
-    uri.query = URI.encode_www_form(params)
-    res = Net::HTTP.get_response(uri)
-    if res.is_a?(Net::HTTPSuccess)
-      point_json = res.body
-      point_json = JSON.parse(point_json)
-      points_position << point_json["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"]["pos"].split(" ")
-    fileout.print(points_position[0][0].to_s + ";" + points_position[0][1].to_s + ";" + points_address[0].to_s + ";" + points_address[0][0].to_s)
-      sleep(1)
-  end
+    geo_code_csv << getLocation(points_address)
+    points_address.clear
+    sleep 0.5
+end
 filein.close
+
+geo_code_csv.each do |row|
+  row.flatten!
+
+  fileout.puts(row)
+end
 fileout.close
 
-
-
-
-  puts points_address[0][0]
-  puts points_position[0][1]
-
-
-end
 
